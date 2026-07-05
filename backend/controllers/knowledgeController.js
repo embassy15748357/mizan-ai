469987159a0d05b5b2d76f085dd68e145b676df3
embassy15748357 <ambassadormdsunusi@gmail.com>
2026-07-05 16:27:05 +0100

    Create knowledgeController.js

---
+++ backend/controllers/knowledgeController.js
@@ -0,0 +1,13 @@
+// Knowledge Controller
+
+exports.searchKnowledge = (req, res) => {
+  res.json({
+    message: "Knowledge search endpoint"
+  });
+};
+
+exports.addKnowledge = (req, res) => {
+  res.json({
+    message: "Add knowledge endpoint"
+  });
+};
