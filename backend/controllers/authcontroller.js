6f649d78a96c9b0f3145b429c993ce93af8adef4
embassy15748357 <ambassadormdsunusi@gmail.com>
2026-07-05 15:19:09 +0100

    Create authcontroller.js

---
+++ backend/controllers/authcontroller.js
@@ -0,0 +1,13 @@
+// Authentication Controller
+
+exports.register = (req, res) => {
+  res.json({
+    message: "User registration endpoint"
+  });
+};
+
+exports.login = (req, res) => {
+  res.json({
+    message: "User login endpoint"
+  });
+};
