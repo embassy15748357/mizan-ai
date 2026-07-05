9b23344fa839981f715698e3c0439145c92d439c
embassy15748357 <ambassadormdsunusi@gmail.com>
2026-07-05 16:34:42 +0100

    Create userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,13 @@
+// User Controller
+
+exports.getProfile = (req, res) => {
+  res.json({
+    message: "User profile endpoint"
+  });
+};
+
+exports.updateProfile = (req, res) => {
+  res.json({
+    message: "Update user profile endpoint"
+  });
+};
